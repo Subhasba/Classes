5865a3230d4c0b062a48dc04b17e42888d756a6e
Subhasba <makeshsubha111@gmail.com>
2025-07-10 12:10:51 +0530

    NewClass.js file added

--- NewClass.js
+++ NewClass.js
@@ -1,1 +1,48 @@
-
+class School {
+    constructor(name) {
+        this.school = name
+    }
+    display() {
+        console.log(`School Name:${this.school}`);
+    }
+}
+class Clazz {
+    constructor(name) {
+        this.clazz = name;
+    }
+    display() {
+        console.log(`Class:${this.clazz}`);
+    }
+}
+class Section {
+    constructor(name) {
+        this.section = name;
+    }
+    display() {
+        console.log(`Section:${this.section}`);
+    }
+}
+class Student {
+    constructor(name, age) {
+        this.student = name;
+        this.studentage = age;
+    }
+    display() {
+        console.log(`Student Name : ${this.student}`);
+        console.log(`Student Age : ${this.studentage}`);
+    }
+}
+const school = new School("Antony School")
+const clazz = new Clazz(11)
+const clazz2 = new Clazz(12)
+const section = new Section("A")
+const sec2 = new Section("B")
+const student = new Student("Subha", 18)
+const student2 = new Student("Dhivya", 18)
+school.display();
+clazz.display();
+section.display();
+student.display();
+clazz2.display();
+sec2.display();
+student2.display();
